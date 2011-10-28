046d7c0986ff13f6907c3b75faeb2632672c2d36
saloni09066 <saloni09066@iiitd.ac.in>
2011-10-28 07:13:06 +0000

    1st.. :D

---
+++ src/nik.java
@@ -0,0 +1,12 @@
+
+public class nik {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nikhil is my bestest brother");
+	}
+
+}
