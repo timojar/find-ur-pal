573dac920049c599a7dc77a3a0d17e2193601479
saloni09066@iiitd.ac.in <saloni09066@iiitd.ac.in@abb6f1af-820d-59bd-8cb4-d3209994da22>
2011-10-28 14:39:56 +0000

    File Read with 4 tags.

---
+++ src/SendMsg.java
@@ -0,0 +1,13 @@
+
+public class SendMsg {
+
+	public void send()
+	{
+		System.out.println("Msg sent to B");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
