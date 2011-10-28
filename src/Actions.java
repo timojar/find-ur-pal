fe642248f15ef33591d8cfe8f6f86c433a86e3bb
saloni09066 <saloni09066@iiitd.ac.in>
2011-10-28 14:39:56 +0000

    File Read with 4 tags.

--- src/Actions.java
+++ src/Actions.java
@@ -1,5 +1,74 @@
 import java.io.*;
+import java.sql.*;
 public class Actions {
-	
+	public Connection conn = null;
+	public String url = "jdbc:mysql://localhost:3306/";
+	public String dbName = "temp";
+	public String driver = "com.mysql.jdbc.Driver";
+	public String userName = "root";
+	public String password = "saloni";
+	public void updateConnections(String num1,String num2)
+	{
+		//jdbcConnect();
+		System.out.println("inside action1");
+		String query = "insert into connections (personA,personB) values (" + num1 + "," + num2 + ")";
+		try
+		{
+			System.out.println("inside try");
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url+dbName,userName,password);
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(query);
+			conn.close();
+			System.out.println("Disconnected from database");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void updateFollow(String num1, String num2)
+	{
+		System.out.println("inside action2");
+		String query = "insert into follow (personA,personB) values (" + num1 + "," + num2 + ")";
+		try
+		{
+			System.out.println("inside try");
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url+dbName,userName,password);
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(query);
+			conn.close();
+			System.out.println("Disconnected from database");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void updateLocation(String num,String place)
+	{
+		System.out.println("inside action2");
+		String query = "insert into location (person,place) values (" + num + "," + place + ")";
+		try
+		{
+			System.out.println("inside try");
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url+dbName,userName,password);
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(query);
+			conn.close();
+			System.out.println("Disconnected from database");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args)
+	{
+		Actions a = new Actions();
+		a.updateConnections("9968379126", "9868224419");
+	}
 
 }
--- src/FileRead.java
+++ src/FileRead.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class FileRead {
 	public int findtag(String filename){
@@ -27,6 +28,14 @@ public class FileRead {
 				if(char3 == '2')
 				{
 					return 2;
+				}
+				if(char3 == '3')
+				{
+					return 3;
+				}
+				if(char3 == '4')
+				{
+					return 4;
 				}
 				
 			}
@@ -55,7 +64,7 @@ public class FileRead {
 			String [] list = dir.list();
 			for (int i = 0; i < list.length; i++) 
 			{
-				File f = new File(dir + "/" + list[i]);
+				File f = new File(dir + "\\" + list[i]);
 				if(f.isFile())
 				{
 					files.add(f.toString());
@@ -71,18 +80,71 @@ public class FileRead {
 		return files.toString();
 	}
 	
+	public ArrayList<String> CreateStringList(String ... values)
+	{
+	    ArrayList<String> results = new ArrayList<String>();
+	    Collections.addAll(results, values);
+	    return results;
+	}
+	public String[] ConvertToStringArray(ArrayList list)
+	{
+	    return (String[])list.toArray(new String[0]);
+	}
+	public String[] AppendItemToArray(String[] array, String newValue)
+	{
+	    ArrayList<String> list = CreateStringList(array);
+	    list.add(array.length, newValue);
+	    return ConvertToStringArray(list);
+	}
+	
 	public static void main(String[] args) {
 		FileRead f = new FileRead();
 		File directory = new File("C:\\Users\\user\\workspace\\findurpalol\\src\\Incoming");
 		String allfiles = f.filelist(directory);
 		String [] list = allfiles.split(",");
 		
-		/*
 		for (int i = 0 ; i < list.length; i++)
 		{
 			System.out.println(list[i]);
 		}
-		*/
+		
+		String [] pf1 = null;
+		String [] pf2 = null;
+		String [] pf3 = null;
+		String [] pf4 = null;
+		if(list.length != 0)
+		{
+		for(int i = 0; i < list.length; i++)
+		{
+			int j = f.findtag(list[i]);
+			if(j == 1)
+				f.AppendItemToArray(pf1, list[i]);
+			else if(j == 2)
+				f.AppendItemToArray(pf2, list[i]);
+			else if(j == 3)
+				f.AppendItemToArray(pf3, list[i]);
+			else if(j == 4)
+				f.AppendItemToArray(pf4, list[i]);
+		}
+		
+		for (int i = 0 ; i < pf1.length; i++)
+		{
+			System.out.println(pf1[i]);
+		}
+		for (int i = 0 ; i < pf2.length; i++)
+		{
+			System.out.println(pf2[i]);
+		}
+		for (int i = 0 ; i < pf3.length; i++)
+		{
+			System.out.println(pf3[i]);
+		}
+		for (int i = 0 ; i < pf4.length; i++)
+		{
+			System.out.println(pf4[i]);
+		}
+		
+		}
 		
 	}
 }
